e999cbbc628738bd9f80306b336656715f47fb7f
ANasser <fawzy3171@gmail.com>
2023-03-10 19:59:35 +0200

    res(Theme): Add custom theme with the new font Montserrat

---
+++ lib/styles/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const secondaryColor = Color(0xFFF8D458);
+const primaryColor = Color(0xFF17181B);
---
+++ lib/styles/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:platform_channels_challenge/styles/colors.dart';
+
+TextTheme? get customTextTheme {
+  final TextTheme base = Typography
+      .material2018()
+      .black;
+  return base.apply(
+    fontFamily: 'Montserrat',
+    displayColor: primaryColor,
+    bodyColor: primaryColor,
+  );
+}
+
+ThemeData darkTheme = ThemeData(
+  fontFamily: 'Montserrat',
+  primaryColor: primaryColor,
+  colorScheme: const ColorScheme.dark(
+    primary: primaryColor,
+    secondary: secondaryColor,
+  ),
+);
